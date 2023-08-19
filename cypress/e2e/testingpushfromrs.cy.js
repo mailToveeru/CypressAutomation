d4564b4145be64481a8d57d2be6beaf05a8c37bc
Rahul Handifod <rahul.handifod@rakuten.com>
2023-08-19 13:03:56 +0530

    file add

--- cypress/e2e/testingpushfromrs.cy.js
+++ cypress/e2e/testingpushfromrs.cy.js
@@ -1,20 +1,43 @@
 class Solution {
-    public int solution(int[] A) {
-        int n = A.length;
-        boolean[] seen = new boolean[n + 1];
-
-        for (int num : A) {
-            if (num > 0 && num <= n) {
-                seen[num] = true;
+    public String solution(int[] T) {
+        int n = T.length;
+        int seasonLength = n / 4;
+        
+        int[] amplitudes = new int[4];
+        
+        for (int i = 0; i < 4; i++) {
+            int startIndex = i * seasonLength;
+            int endIndex = startIndex + seasonLength;
+            
+            int maxTemp = Integer.MIN_VALUE;
+            int minTemp = Integer.MAX_VALUE;
+            
+            for (int j = startIndex; j < endIndex; j++) {
+                maxTemp = Math.max(maxTemp, T[j]);
+                minTemp = Math.min(minTemp, T[j]);
+            }
+            
+            amplitudes[i] = maxTemp - minTemp;
+        }
+        
+        int maxAmplitudeIndex = 0;
+        for (int i = 1; i < 4; i++) {
+            if (amplitudes[i] > amplitudes[maxAmplitudeIndex]) {
+                maxAmplitudeIndex = i;
             }
         }
-
-        for (int i = 1; i <= n; i++) {
-            if (!seen[i]) {
-                return i;
-            }
+        
+        switch (maxAmplitudeIndex) {
+            case 0:
+                return "WINTER";
+            case 1:
+                return "SPRING";
+            case 2:
+                return "SUMMER";
+            case 3:
+                return "AUTUMN";
+            default:
+                return "";
         }
-
-        return n + 1;
     }
 }
