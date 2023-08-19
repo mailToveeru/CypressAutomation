c50ffad9288469b8c02c8e34abc0cd4c94a16417
Rahul Handifod <rahul.handifod@rakuten.com>
2023-08-19 13:21:54 +0530

    file add

--- cypress/e2e/testingpushfromrs.cy.js
+++ cypress/e2e/testingpushfromrs.cy.js
@@ -1,43 +1,47 @@
-class Solution {
-    public String solution(int[] T) {
-        int n = T.length;
-        int seasonLength = n / 4;
-        
-        int[] amplitudes = new int[4];
-        
-        for (int i = 0; i < 4; i++) {
-            int startIndex = i * seasonLength;
-            int endIndex = startIndex + seasonLength;
-            
-            int maxTemp = Integer.MIN_VALUE;
-            int minTemp = Integer.MAX_VALUE;
-            
-            for (int j = startIndex; j < endIndex; j++) {
-                maxTemp = Math.max(maxTemp, T[j]);
-                minTemp = Math.min(minTemp, T[j]);
-            }
-            
-            amplitudes[i] = maxTemp - minTemp;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTests {
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        // Create a new instance of ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to the application's main screen
+        driver.get("url_of_your_application");
+
+        // Check if email and password fields are present
+        WebElement emailInput = driver.findElement(By.id("email-input"));
+        WebElement passwordInput = driver.findElement(By.id("password-input"));
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+
+        // Perform tests based on the requirements
+        // Test 1
+        if (emailInput.isDisplayed() && passwordInput.isDisplayed() && loginButton.isDisplayed()) {
+            System.out.println("Test 1 Passed: Email and password fields are present.");
+        } else {
+            System.out.println("Test 1 Failed: Email and password fields are not present.");
         }
-        
-        int maxAmplitudeIndex = 0;
-        for (int i = 1; i < 4; i++) {
-            if (amplitudes[i] > amplitudes[maxAmplitudeIndex]) {
-                maxAmplitudeIndex = i;
-            }
+
+        // Test 2
+        emailInput.sendKeys("login@codility.com");
+        passwordInput.sendKeys("valid_password");
+        loginButton.click();
+
+        WebElement successMessage = driver.findElement(By.cssSelector(".message.success"));
+        if (successMessage.isDisplayed() && successMessage.getText().contains("Welcome to Codility")) {
+            System.out.println("Test 2 Passed: Successful login message is visible.");
+        } else {
+            System.out.println("Test 2 Failed: Successful login message is not visible.");
         }
-        
-        switch (maxAmplitudeIndex) {
-            case 0:
-                return "WINTER";
-            case 1:
-                return "SPRING";
-            case 2:
-                return "SUMMER";
-            case 3:
-                return "AUTUMN";
-            default:
-                return "";
-        }
+
+        // Repeat similar steps for other tests (3, 4, and 5)
+
+        // Close the browser
+        driver.quit();
     }
 }
